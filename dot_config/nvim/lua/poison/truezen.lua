2e15c6997489be7e800f4239c35b2fc7b15a6b2a
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2022-07-31 21:35:13 +0100

    true-zen.nvim padding & width

--- dot_config/nvim/lua/poison/truezen.lua
+++ dot_config/nvim/lua/poison/truezen.lua
@@ -3,4 +3,16 @@ if not status_ok then
   return
 end
 
-true_zen.setup()
+true_zen.setup({
+  modes = {
+    ataraxis = {
+      minimum_writting_area = {
+        width = 80,
+      },
+      padding = {
+        left = 32,
+        right = 32,
+      }
+    }
+  }
+})
