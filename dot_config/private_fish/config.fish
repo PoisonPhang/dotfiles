4ca8d1508311f0e9f7fb663c8dff32c279bcaa46
Connor Davis <17688291+PoisonPhang@users.noreply.github.com>
2022-08-17 10:18:38 -0500

    source nix w/ bass

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -19,4 +19,8 @@ set -gx EDITOR nvim
 # STARSHIP
 starship init fish | source
 
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/poisonphang/.ghcup/bin # ghcup-env
+# ghcup-env
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/poisonphang/.ghcup/bin 
+
+# nix
+bass source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
