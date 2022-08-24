baba9d6fe9052ad98ccca4c2983ecdd9b21946e1
Connor Davis <17688291+PoisonPhang@users.noreply.github.com>
2022-08-24 09:05:16 -0500

    Wezterm use zsh

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -8,6 +8,7 @@ local font = wezterm.font_with_fallback({
 
 return {
   color_scheme = "Catppuccin",
+  default_prog = { '/usr/bin/zsh', '-l' },
   font = font,
   font_size = 16,
   hide_tab_bar_if_only_one_tab = true,
